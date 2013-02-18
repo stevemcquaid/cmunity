bd0dcbc8b36e41a5647c65604fbe1ea42a9c7077
Steve McQuaid <steve@stevemcquaid.com>
2013-02-18 17:10:48 -0500

    Updates to Homepage to show all posts on front page of website. In the middle of working, laptop about to die

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
+  # Want to show all the posts of all types on the homepage for MVP
   def index
+    @contents = Content.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @contents }
+    end
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Cmunity::Application.routes.draw do
   get "home/index"
   
-  #You can have the root of your site routed with "root"
+  # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'home#index'
 
