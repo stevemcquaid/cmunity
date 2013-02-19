8bfad1cd01b23b4ae0a5ff850bfe8b7a30b07a20
Steve McQuaid <steve@stevemcquaid.com>
2013-02-19 03:29:58 -0500

    Contents updates to merge in foreign associations displaying names, not foreign key ids. Minor layout sutff. Need to do same thing to other models and scope out home page.

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,6 +1,6 @@
 class Content < ActiveRecord::Base
-  belongs_to :group
-  belongs_to :user
+  belongs_to :group, :class_name => "Group", :foreign_key => :parent_group_id
+  belongs_to :user, :class_name => "User", :foreign_key => :creator_id
   has_many :text_posts, :dependent => :destroy
   has_many :url_posts, :dependent => :destroy
   has_many :image_posts, :dependent => :destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,24 @@
 class User < ActiveRecord::Base
+  # Associations
+  # -----------------------------
   has_many :contents
   has_many :user_groups
   has_many :groups, :through => :user_groups
+  
+  # Scopes
+  # -----------------------------
+  scope :alphabetical, order("last_name, first_name")
+  
+  # Validations
+  # -----------------------------
+  # make sure required fields are present
+  validates_presence_of :first_name, :last_name, :email  
+  validates_uniqueness_of :email, :allow_blank => true
+  validates_format_of :email, :with => /^[\w]([^@\s,;]+)@(([a-z0-9.-]+\.)+(com|edu|org|net|gov|mil|biz|info))$/i, :message => "is not a valid format", :allow_blank => true
+  #need more validations when adding password + signed up, email validated, and authorized etc.
+  
+  def name
+    first_name + " " + last_name
+  end
   
 end
