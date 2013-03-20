78490b0ebe94f529808e95d45d4eb531ad3d6f91
Steve McQuaid <steve@stevemcquaid.com>
2013-03-20 10:31:36 -0400

    unstable - just stashing stuff … need to setup accessor for current_user

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,10 @@ class Group < ActiveRecord::Base
   has_many :user_groups
   has_many :users, :through => :user_groups
   
+  before_create :add_user_to_group
+
+   private
+   def add_user_to_group
+     user_groups.build(:user => current_user, :group => self)
+   end
 end
