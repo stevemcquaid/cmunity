cf1640595dacde31b5751744247531d19d6c6f43
Sean Hoffman <IsRaz88@gmail.com>
2013-04-06 00:28:17 -0400

    Added more association definitions to models

--- app/models/content.rb
+++ app/models/content.rb
@@ -4,6 +4,6 @@ class Content < ActiveRecord::Base
   belongs_to :group, :class_name => "Group", :foreign_key => :parent_group_id
   belongs_to :creator, :class_name => "User", :foreign_key => :creator_id
   
-  attr_accessible :title, :creator_id, :parent_group_id, :is_private, :description
+  attr_accessible :title, :creator_id, :parent_group_id, :is_private, :description, :mediable_id, :mediable_type
 
 end
