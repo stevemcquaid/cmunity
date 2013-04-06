cbd46589cc8f84feb7d68857f489630e0367dd23
Steve McQuaid <steve@stevemcquaid.com>
2013-04-06 02:53:04 -0400

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130331214423) do
+ActiveRecord::Schema.define(:version => 20130404192410) do
 
   create_table "activities", :force => true do |t|
     t.integer  "owner_id"
@@ -77,6 +77,14 @@ ActiveRecord::Schema.define(:version => 20130331214423) do
   add_index "memberships", ["group_id"], :name => "index_memberships_on_group_id"
   add_index "memberships", ["user_id"], :name => "index_memberships_on_user_id"
 
+  create_table "ratings", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "content_id"
+    t.integer  "vote_value"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "roles", :force => true do |t|
     t.string   "name"
     t.integer  "resource_id"
@@ -128,6 +136,8 @@ ActiveRecord::Schema.define(:version => 20130331214423) do
     t.datetime "avatar_updated_at"
   end
 
+  add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
