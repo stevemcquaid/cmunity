74033b6ef5facbead4249a117b17381d54c5d542
Sean Hoffmam <IsRaz88@gmail.com>
2013-04-10 13:08:47 -0400

    Updated Gemfile and deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,7 @@ set :user, "deploy"
 set :deploy_to, "/home/#{user}/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
+set :rake, "bundle exec rake"
 
 set :scm, "git"
 set :repository, "git@github.com:stevemcquaid/#{application}.git"
@@ -22,12 +23,22 @@ ssh_options[:forward_agent] = true
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
 namespace :deploy do
-    task :start, :roles => :app do
+  
+  desc "Restarts Passenger"
+  task :start, :roles => :app do
     run "touch #{deploy_to}/current/tmp/restart.txt"
   end
+  
   task :stop do; end
+  
   task :restart, roles: :app, except: {no_release: true} do
     run "touch #{deploy_to}/current/tmp/restart.txt"
+  end
+
+  desc "Sets permissions for Rails Application"
+  task :set_permissions do
+    puts "\n\n=== Setting Permissions! ===\n\n"
+    run "chown -R www-data:www-data #{deploy_to}"
   end
 
   task :setup_config, roles: :app do
@@ -51,5 +62,69 @@ namespace :deploy do
       exit
     end
   end
+
+  desc "Run a command on the remote server. Specify command='my_command'."
+  task :run_command do
+    run "cd #{current_path}; #{ENV['command']}"
+  end
+  
   before "deploy", "deploy:check_revision"
+
+  # Datbase Tasks
+  namespace :db do
+
+    desc "Syncs the database.yml file from the local machine to the remote machine"
+    task :sync_yaml do
+      puts "\n\n=== Syncing database yaml to the production server! ===\n\n"
+      unless File.exist?("config/database.yml")
+        puts "There is no config/database.yml.\n "
+        exit
+      end
+      system "rsync -vr --exclude='.DS_Store' config/database.yml #{user}@#{application}:#{shared_path}/config/"
+    end
+    
+    desc "Create Production Database"
+    task :create do
+      puts "\n\n=== Creating the Production Database! ===\n\n"
+      run "cd #{current_path}; rake db:create RAILS_ENV=production"
+      system "cap deploy:set_permissions"
+    end
+
+    desc "Migrate Production Database"
+    task :migrate do
+      puts "\n\n=== Migrating the Production Database! ===\n\n"
+      run "cd #{current_path}; rake db:migrate RAILS_ENV=production"
+      system "cap deploy:set_permissions"
+    end
+
+    desc "Resets the Production Database"
+    task :migrate_reset do
+      puts "\n\n=== Resetting the Production Database! ===\n\n"
+      run "cd #{current_path}; rake db:migrate:reset RAILS_ENV=production"
+    end
+    
+    desc "Destroys Production Database"
+    task :drop do
+      puts "\n\n=== Destroying the Production Database! ===\n\n"
+      run "cd #{current_path}; rake db:drop RAILS_ENV=production"
+      system "cap deploy:set_permissions"
+    end
+
+    desc "Moves the SQLite3 Production Database to the shared path"
+    task :move_to_shared do
+      puts "\n\n=== Moving the SQLite3 Production Database to the shared path! ===\n\n"
+      run "mv #{current_path}/db/production.sqlite3 #{shared_path}/db/production.sqlite3"
+      system "cap deploy:setup_symlinks"
+      system "cap deploy:set_permissions"
+    end
+
+    desc "Populates the Production Database"
+    task :seed do
+      puts "\n\n=== Populating the Production Database! ===\n\n"
+      run "cd #{current_path}; rake db:seed RAILS_ENV=production"
+    end
+
+  end
+
+
 end
