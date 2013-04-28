9ef780c2b534603a35add88e15f2919dc9eb096e
Stephany Park <park.stephany@gmail.com>
2013-04-28 13:57:54 -0400

    Stephany updated approval test

--- test/unit/approval_test.rb
+++ test/unit/approval_test.rb
@@ -11,4 +11,15 @@ class ApprovalTest < ActiveSupport::TestCase
 
 #attr_accessible :approvable_id, :approvable_type, :group_id, :received_at, :replied_at, :sender_id, :sender_type, :sent_at, :status
 	
+	test "attr_accessible" do
+		allow_mass_assignment_of(:approvable_id)
+		allow_mass_assignment_of(:approvable_type)
+		allow_mass_assignment_of(:group_id)
+		allow_mass_assignment_of(:received_at)
+		allow_mass_assignment_of(:replied_at)
+		allow_mass_assignment_of(:sender_id)
+		allow_mass_assignment_of(:sender_type)
+		allow_mass_assignment_of(:sent_at)
+		allow_mass_assignment_of(:status)
+	end
 end
