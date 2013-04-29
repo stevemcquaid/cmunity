fd81431a76a48fde7ed33c39704b1facce9240f0
Steve McQuaid <steve@stevemcquaid.com>
2013-04-29 12:24:28 -0400

    seees attempt fixing

--- app/models/text_post.rb
+++ app/models/text_post.rb
@@ -1,11 +1,11 @@
 class TextPost < ActiveRecord::Base
 
   has_one :content, :as => :mediable, :dependent => :destroy
-  has_one :group, :through => :content, :foreign_key => 'parent_group_id'
+  has_one :group, :through => :content, :foreign_key => :parent_group_id
   has_one :creator, :through => :content
   has_many :activities, :as => :trackable
   
   accepts_nested_attributes_for :content
-  attr_accessible :text, :content_attributes
+  attr_accessible :text, :content_attributes, :title, :creator_id, :parent_group_id, :is_private
 
 end
